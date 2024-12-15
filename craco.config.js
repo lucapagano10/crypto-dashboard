d819c33a69358326c29bcc612c8a69e3b0c0a338
luca pagano <lpagano@udesa.edu.ar>
2024-12-15 17:18:44 -0300

    Added crypto polyfills and CRACO configuration

---
+++ craco.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  webpack: {
+    configure: {
+      resolve: {
+        fallback: {
+          crypto: require.resolve('crypto-browserify'),
+          stream: require.resolve('stream-browserify'),
+          buffer: require.resolve('buffer/'),
+        },
+      },
+    },
+  },
+};
